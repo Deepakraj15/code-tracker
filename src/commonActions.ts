3df6bde4d77aa66660f07cef5561acba841c9ee6
Deepakraj15 <deepakraj15112002@gmail.com>
2025-01-11 12:23:26 +0530

    added handleGitHubActions

--- src/commonActions.ts
+++ src/commonActions.ts
@@ -1,19 +1,45 @@
-import { fetchUserData, saveDataToDb } from "./handleDBactions";
-import { IAccount } from "./utils/interfaces";
 import * as vscode from "vscode";
+import fs from "fs";
+import path from "path";
+import { CONFIG_FILE, VSCODE_FOLDER } from "./utils/constants";
+import { createConfigFile, getCurrentWorkingDirectory, isConfigFileExist } from "./handleFileActions";
 
+export const checkUserRecord = async (authToken: vscode.AuthenticationSession) => {
+    const currentWorkingDirectory = getCurrentWorkingDirectory();
+    await isConfigFileExist(currentWorkingDirectory) ? {} : createConfigFile(currentWorkingDirectory, authToken);
 
-export const checkIsOldUser = (authToken:  vscode.AuthenticationSession) => {
-    const account: IAccount = authToken.account;
-    const result = fetchUserData(account);
-    if (result === null) {
-        vscode.window.showWarningMessage("Oops.. User not found .Do you want to register?", "Proceed", "Cancel").then((selection) => {
-            if (selection === "Proceed") {
-                saveDataToDb(account);
-                return true;
-            }
-            return false;
-        }); 
+    if (currentWorkingDirectory !== undefined) {
+        const vscodeFolderPath = path.join(currentWorkingDirectory.uri.fsPath, VSCODE_FOLDER);
+        const configFilePath = path.join(vscodeFolderPath, CONFIG_FILE);
+        try {
+            const data = await new Promise<any>((resolve, reject) => {
+                fs.readFile(configFilePath, 'utf8', (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data); 
+                    }
+                });
+            });
+            return JSON.parse(data);
+
+        } catch (err) {
+            // Delete the config file and create a new one
+            fs.unlinkSync(configFilePath);
+            vscode.window.showErrorMessage("Oops...Something went wrong. Try again.");
+            return null;
+        }
     }
-    return true;
+    return null; 
 };
+
+
+export const handleGitHubActions = () => {
+	/**
+     * 1) check if repo is already available 
+     * 2) if repo is not available create new repo
+     * 3) if repo is already available start pushing the code
+     */
+};
+
+
