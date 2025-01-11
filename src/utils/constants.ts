f52963778fd800dc6c0942f7f3d5e910f672a5f6
Deepakraj15 <deepakraj15112002@gmail.com>
2025-01-11 12:22:14 +0530

    made few changes on the constants added constants for vscode file management

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const CONFIG_FILE = "codeTracker.config.json";
+export const VSCODE_FOLDER = '.vscode';
+export const CONFIG_FILE_PATTERN = `**/${VSCODE_FOLDER}/${CONFIG_FILE}`;
+export const CODE_TRACKER_SUFFIX = '-code-tracker';
